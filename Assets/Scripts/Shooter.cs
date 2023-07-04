a043742c261acad890102a450b918dcad310b83c
mirmac6 <mirkomacanovic@yahoo.com>
2023-07-05 00:08:06 +0200

    First version

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileLifetime = 5f;
+    [SerializeField] float baseFiringRate = 0.2f;
+
+    [Header("AI")]
+    [SerializeField] bool useAI;
+    [SerializeField] float firingRateVariance = 0f;
+    [SerializeField] float minFiringRate = 0.1f;
+    Coroutine firinigCoroutine;
+
+    [HideInInspector] public bool isFiring;
+    AudioPlayer audioPlayer;
+    void Awake() 
+    {
+        audioPlayer = FindObjectOfType<AudioPlayer>();    
+    }
+    void Start()
+    {
+        if(useAI)
+        {
+            isFiring = true;
+        }
+    }
+
+    void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        if(isFiring && firinigCoroutine == null)
+        {
+            firinigCoroutine = StartCoroutine(FireContinuously());
+        }else if(!isFiring && firinigCoroutine != null)
+        {
+            StopCoroutine(firinigCoroutine);
+            firinigCoroutine = null;
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            GameObject instance = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            
+            Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+            if(rb != null)
+            {
+                rb.velocity = transform.up * projectileSpeed;
+            }
+            
+            Destroy(instance, projectileLifetime);
+
+            float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance, baseFiringRate + firingRateVariance);
+
+            timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minFiringRate, float.MaxValue);
+
+            audioPlayer.PlayShootingClip();
+            
+            yield return new WaitForSeconds(timeToNextProjectile);
+        }
+    }
+}
